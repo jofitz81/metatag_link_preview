24f04b8be383e7685548ddc6d4ff4f467f80a73e
Jo Fitzgerald <jofitz@gmail.com>
2025-05-09 17:13:13 +0100

    Create Twitter Card Link Preview plugin in a sub-module
    …including template and styling

---
+++ metatag_link_preview_twitter/src/Plugin/LinkPreview/Twitter.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\metatag_link_preview_twitter\Plugin\LinkPreview;
+
+use Drupal\Component\Utility\Html;
+use Drupal\metatag_link_preview\Annotation\LinkPreview;
+use Drupal\metatag_link_preview\LinkPreviewPluginBase;
+
+/**
+ * Plugin implementation of the link_preview.
+ *
+ * @LinkPreview(
+ *   id = "twitter",
+ *   label = @Translation("X"),
+ *   description = @Translation("An indication of how the page will appear when shared to X (formerly Twitter).")
+ * )
+ */
+class Twitter extends LinkPreviewPluginBase {
+
+  /**
+   * @inheritDoc
+   */
+  public function card(array $meta_tags): array {
+    return [
+      '#theme' => 'twitter_card',
+      '#title' => $meta_tags['title'],
+      '#image' => $meta_tags['og_image'],
+      '#attached' => ['library' => ['metatag_link_preview_twitter/metatag_link_preview_twitter']],
+    ];
+  }
+
+}
